1f6df32137880587f4aec06e3354dbff92ce3af5
Dhannyphantom <64708060+Dhannyphantom@users.noreply.github.com>
2022-10-03 23:17:38 +0100

    Added a Header component to the general Layout

---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Redux Blog</h1>
+      <nav>
+        <ul>
+          <li>
+            <Link className="link" to="/">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link className="link" to="/post">
+              New Post
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
