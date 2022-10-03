c84a1988ff2b8ef31afaa9f44b6c8b99bd1e033e
Dhannyphantom <64708060+Dhannyphantom@users.noreply.github.com>
2022-10-03 22:07:50 +0100

    Added react-router-dom and hooked it up with the project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import "./App.css";
 import PostForm from "./logic/posts/PostForm";
 import PostRender from "./logic/posts/PostRender";
+import { Routes, Route } from "react-router-dom";
+import { Layout } from "./components/Layout";
+import SinglePostPage from "./components/SinglePostPage";
 
 function App() {
   return (
-    <div className="App">
-      <PostForm />
-      <PostRender />
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<PostRender />} />
+        <Route path="/post">
+          <Route index element={<PostForm />} />
+          <Route path=":/postId" element={<SinglePostPage />} />
+        </Route>
+      </Route>
+    </Routes>
   );
 }
 
--- src/logic/posts/PostDetail.js
+++ src/logic/posts/PostDetail.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import PostAuthor from "./PostAuthor";
 import PostReactions from "./PostReactions";
@@ -8,8 +9,9 @@ const PostDetail = ({ post }) => {
   return (
     <article>
       <h4> {post.title} </h4>
-      <p> {post.body.substring(0, 100)} </p>
+      <p> {post.body.substring(0, 75)}... </p>
       <small>
+        <Link to={`post/${post.id}`}>View Post</Link>
         <PostAuthor userId={post.userId} />
         <TimeAgo timestamp={post.date} />
       </small>
