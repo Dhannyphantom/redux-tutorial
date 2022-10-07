0eb5295b2851ca1e1a61f83846660ffe7d544433
Dhannyphantom <64708060+Dhannyphantom@users.noreply.github.com>
2022-10-07 14:13:41 +0100

    Prepared project for the todos app using RTK query

--- src/App.js
+++ src/App.js
@@ -1,33 +1,10 @@
 import "./App.css";
-import PostForm from "./logic/posts/PostForm";
-import PostRender from "./logic/posts/PostRender";
-import { Routes, Route, Navigate } from "react-router-dom";
-import { Layout } from "./components/Layout";
-import SinglePostPage from "./logic/posts/SinglePostPage";
-import EditPostPage from "./logic/posts/EditPostPage";
-import UsersList from "./logic/users/UsersList";
-import UserPage from "./logic/users/UserPage";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<PostRender />} />
-        <Route path="post">
-          <Route index element={<PostForm />} />
-          <Route path=":postId" element={<SinglePostPage />} />
-          <Route path="edit/:postId" element={<EditPostPage />} />
-        </Route>
-
-        <Route path="users">
-          <Route index element={<UsersList />} />
-          <Route path=":userId" element={<UserPage />} />
-        </Route>
-
-        {/*   CATCH ALL ROUTES   */}
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Route>
-    </Routes>
+    <main className="App">
+      <h1>Todos</h1>
+    </main>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,25 +3,11 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { store } from "./app/store";
-import { Provider } from "react-redux";
-import { fetchUsers } from "./logic/users/usersSlice";
-import { fetchPosts } from "./logic/posts/postSlice";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-store.dispatch(fetchUsers());
-store.dispatch(fetchPosts());
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <Router>
-        <Routes>
-          <Route path="/*" element={<App />} />
-        </Routes>
-      </Router>
-    </Provider>
+    <App />
   </React.StrictMode>
 );
 
